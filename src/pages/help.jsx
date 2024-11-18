fb43f71ac806ab7401ca5e18b05e3544f7f803b4
moulik-1612 <mouliktiwari1612@gmail.com>
2024-11-18 11:31:27 +0530

    replace anchor tag with navlink

--- src/pages/help.jsx
+++ src/pages/help.jsx
@@ -1,3 +1,4 @@
+import { NavLink } from "react-router-dom";
 import Footer from "../Landing Page/Footer";
 import Navbar from "../Landing Page/Navbar";
 
@@ -19,11 +20,11 @@ function Help() {
                 <li><strong>Problems with accessing content on mobile devices?</strong></li>
             </ul>
 
-            <p>If you encounter any of these issues or other technical difficulties, please reach out to us through the <a href="contact.html">Contact Us</a> page. You can share your concerns, and we’ll get back to you as soon as possible.</p>
+            <p>If you encounter any of these issues or other technical difficulties, please reach out to us through the <NavLink to='/contact'>Contact Us</NavLink> page. You can share your concerns, and we’ll get back to you as soon as possible.</p>
 
             <p><strong>Want to help us improve?</strong><br/>We’re always looking for ways to make the platform better. Feel free to send your suggestions and ideas on how we can enhance the learning experience.</p>
 
-            <p>Visit our <a href="contact.html">Contact Us</a> page and let us know!</p>
+            <p>Visit our <NavLink to='/contact'>Contact Us</NavLink> page and let us know!</p>
         </div>
     </section>
     <Footer />
